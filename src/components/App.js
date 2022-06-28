d1d5cd841a95c9f910e11d7145865dedd64f57e4
fahimefto <ifto.141.fi@gmail.com>
2022-06-28 19:36:22 +0600

    private routen & public route fixed

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,8 @@ import Login from "./pages/Login";
 import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
 import { AuthProvider } from "../context/AuthContext";
+import PrivateOutlet from "./PrivateOutlet";
+import PublicOutlet from "./PublicOutlet";
 
 function App() {
   return (
@@ -14,11 +16,16 @@ function App() {
       <AuthProvider>
         <Layout>
           <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/signup" element={<Signup />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/quiz" element={<Quiz />} />
-            <Route exact path="/result" element={<Result />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/*" element={<PublicOutlet />}>
+              <Route path="signup" element={<Signup />} />
+              <Route path="login" element={<Login />} />
+            </Route>
+
+            <Route path="/*" element={<PrivateOutlet />}>
+              <Route path="quiz" element={<Quiz />} />
+              <Route path="result" element={<Result />} />
+            </Route>
           </Routes>
         </Layout>
       </AuthProvider>
---
+++ src/components/PrivateOutlet.js
@@ -0,0 +1,7 @@
+import { useAuth } from "../context/AuthContext";
+import { Navigate, Outlet } from "react-router-dom";
+
+export default function PrivateOutlet() {
+  const { currentUser } = useAuth();
+  return currentUser ? <Outlet /> : <Navigate to="/login" />;
+}
